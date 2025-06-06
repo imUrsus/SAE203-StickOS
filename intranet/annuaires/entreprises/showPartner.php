9dfad4b398cbf67c1ba8b1112d74d5e276e8239b
GREHAIGNE loic <loic.grehaigne@etudiant.univ-rennes.fr>
2025-06-06 11:41:22 +0200

    modification de showPartner

--- intranet/annuaires/entreprises/showPartner.php
+++ intranet/annuaires/entreprises/showPartner.php
@@ -1,24 +1,70 @@
-<?php
-$jsonFile = 'data/annuaire_fournisseurs.json';
-$logoFile = 'partnerLogo/';
-$jsonData = file_get_contents($jsonFile);
-$utilisateurs = json_decode($jsonData, true);
-?>
-<table class="table table-bordered table-hover">
-            <thead class="table-dark">
-                <tr>
-                    <th>Nom</th>
-                    <th>Descrition</th>
-                    <th>Logo</th>
-                </tr>
-            </thead>
-            <tbody>
-
-            <?php foreach ($utilisateurs as $user): ?>
-                <tr>
-                    <td><?php echo htmlspecialchars($user['Nom']); ?></td>
-                    <td><?php echo htmlspecialchars($user['Description']); ?></td>
-                    <td><?php echo htmlspecialchars($user['Logo']); ?></td>
-                    <td>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Modifier les partenaires</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css">
+</head>
+<body>
+    <table class="table table-bordered table-hover">
+        <thead class="table-dark">
+            <tr>
+                <th>Nom</th>
+                <th>Description</th>
+                <th>Logo</th>
+                <th>Actions</th>
             </tr>
-<?php endforeach; ?>
+        </thead>
+        <tbody>
+            <?php
+            $jsonFile = 'data/annuaire_fournisseurs.json';
+
+            if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+                $index = $_POST['index'];
+                $nom = $_POST['Nom'];
+                $description = $_POST['Description'];
+
+                $jsonData = file_get_contents($jsonFile);
+                $utilisateurs = json_decode($jsonData, true);
+
+                if ($index >= 0 && $index < count($utilisateurs)) {
+                    $utilisateurs[$index]['Nom'] = $nom;
+                    $utilisateurs[$index]['Description'] = $description;
+
+                    if (isset($_FILES['Logo']) && $_FILES['Logo']['error'] === UPLOAD_ERR_OK) {
+                        $uploadDir = 'partnerLogo/';
+                        $uploadFile = $uploadDir . basename($_FILES['Logo']['name']);
+
+                        if (move_uploaded_file($_FILES['Logo']['tmp_name'], $uploadFile)) {
+                            $utilisateurs[$index]['Logo'] = $uploadFile;
+                        }
+                    }
+
+                    file_put_contents($jsonFile, json_encode($utilisateurs, JSON_PRETTY_PRINT));
+                }
+            }
+            
+            $jsonData = file_get_contents($jsonFile);
+            $utilisateurs = json_decode($jsonData, true);
+
+            foreach ($utilisateurs as $index => $user): ?>
+                <tr>
+                    <form action="" method="post" enctype="multipart/form-data">
+                        <input type="hidden" name="index" value="<?php echo $index; ?>">
+                        <td><input type="text" name="Nom" value="<?php echo htmlspecialchars($user['Nom']); ?>" class="form-control"></td>
+                        <td><input type="text" name="Description" value="<?php echo htmlspecialchars($user['Description']); ?>" class="form-control"></td>
+                        <td class="text-center">
+                            <img src="<?php echo htmlspecialchars($user['Logo']); ?>" alt="Logo" class="img-fluid rounded-circle mx-auto d-block" style="width: 100px; height: 100px;">
+                            <input type="file" name="Logo" class="form-control-file">
+                        </td>
+                        <td>
+                            <button type="submit" class="btn btn-primary">Mettre à jour</button>
+                        </td>
+                    </form>
+                </tr>
+            <?php endforeach; ?>
+        </tbody>
+    </table>
+</body>
+</html>
